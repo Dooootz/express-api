e340c2575fcf0f92ee6ef602d1d554b812e41c1f
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-02-17 13:07:12 +1000

    GET request successful

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const app = require('express')();
 const PORT = 8080;
 
-app.listen()
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+)
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'test tshirt',
+        size: 'large'
+    })
+})
